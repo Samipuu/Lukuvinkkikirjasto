8f1a24aab12918e59d2cc9b9a454045f3814bd99
afroseppo <kobo-kobe@hotmail.com>
2020-11-22 19:06:55 +0200

    TipDao ja kirjan lisäys sekä hakeminen mahdolliseksi

--- ReadingTips/src/main/java/readingtips/database/TipDao.java
+++ ReadingTips/src/main/java/readingtips/database/TipDao.java
@@ -5,10 +5,46 @@
  */
 package readingtips.database;
 
+import java.util.ArrayList;
+import java.util.List;
+import readingtips.Book;
+import readingtips.Tip;
+
 /**
  *
  * @author tiitinha
  */
 public class TipDao {
+    
+    private BookDao bookDao;
+
+    public TipDao() {
+        bookDao = new BookDao();
+    }
+    
+    public List<Tip> getAllTips() {
+        List<Tip> tips = new ArrayList<>();
+        
+        List<Book> books = getAllBooks();
+        
+        tips.addAll(books);
+        
+        return tips;
+    }
+    
+    private List<Book> getAllBooks() {
+        List<Book> books = bookDao.list();
+        
+        return books;
+    }
+    
+    public boolean createTip(Tip tip) {
+        if (tip.getClass() == Book.class) {
+            Book book = (Book) tip;
+            bookDao.create(book);
+            return true;
+        }
+        return false;
+    }
     
 }
