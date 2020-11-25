064bdc32ed93427b83ad1c66f13e687da11f641a
Laura North <laura.north@helsinki.fi>
2020-11-25 18:53:24 +0200

    Video-yksikkötestit 100%

--- ReadingTips/src/test/java/readingtips/VideoTest.java
+++ ReadingTips/src/test/java/readingtips/VideoTest.java
@@ -1,19 +1,55 @@
 package readingtips;
-
+import java.util.List;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class VideoTest {
 
-    //     @Before
-    // public static void setUp() {
-        
-    // }
+    @Test
+    public void urlIsCorrect() {
+        Video video = new Video("Title", "Author", null, null, null, "URL");
+        assertEquals("URL", video.getUrl());
+    }
 
-    // @Test
-    // public void isbnOikein() {
-    //     Book book = new Book("Title", "Description", null, null, null, "isbn");
-    //     assertEquals("isbn", book.getIsbn());
-    // }
+    @Test
+    public void videoGetTitleNull() {
+        Video video = new Video(null, "Author", "Description", null, null, null);
+        assertEquals("", video.getTitle());
+    }
+
+    @Test
+    public void podcastToString() {
+        Video video = new Video("Title", "Author", "Description", null, null, "url");
+        String returnString = "";
+        returnString += "Type: " + "Video";
+        returnString += "\nTitle: " + "Title";
+        returnString += "\nAuthor: " + "Author";        
+        returnString += "\nDescription: " + "Description";
+        returnString += "\nURL: " + "url";
+        assertEquals(returnString, video.toString());
+    }
+
+    @Test
+    public void videoToStringWithName() {
+        List<String> tags = new ArrayList();
+        List<String> courses = new ArrayList();
+        tags.add("eka");
+        courses.add("tira");       
+              
+        Video video = new Video("Name", "Author", "Description", tags, courses, null);
+        String tagsString = video.getTags().toString();
+        String coursesString = video.getCourses().toString();
+        
+        String returnString = "";
+        returnString += "Type: " + "Video";
+        returnString += "\nTitle: " + "Name";
+        returnString += "\nAuthor: " + "Author";        
+        returnString += "\nDescription: " + "Description";
+        returnString += "\nCourses: " + tagsString.substring(1, tagsString.length()-1);
+        returnString += "\nTags: " + coursesString.substring(1, coursesString.length()-1);
+                        
+        assertEquals(returnString, video.toString());
+    }
+
 }
