7f3b10beaef9078df96485bf1c855a6ac4f852b4
Jussi Asp <jussi.asp@helsinki.fi>
2020-12-04 00:34:05 +0200

    downloaded media files scripts updated

--- ReadingTips/downloaded_media_files/download_video_and_audiofiles.sh
+++ ReadingTips/downloaded_media_files/download_video_and_audiofiles.sh
@@ -3,19 +3,22 @@
 # get youtube-dl
 function lataa_youtube_dl() {
     mkdir bin/
-    if [[ "$OS" == "Windows_NT" ]]; then
+    if [[ "$OS" == "Windows_NT" ]]; then # elif [[ "$OSTYPE" == "msys" ]]; then
         # Windows 10 with Msys2
         echo "info: downloading Windows youtube-dl."
         curl -L https://yt-dl.org/latest/youtube-dl.exe -o bin/youtube-dl.exe
         lataa='bin/youtube-dl.exe'
-    else
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # Ubuntu 20.04 and such
         echo "info: downloading Linux youtube-dl."
         curl -L https://yt-dl.org/downloads/latest/youtube-dl -o bin/youtube-dl
         chmod a+rx bin/youtube-dl
         lataa='bin/youtube-dl'
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "jyy. osx."
     fi
 }
+
 function lataile_videoita() {
     mkdir videos/
     while IFS= read -r line
--- ReadingTips/downloaded_media_files/play_some_videos.sh
+++ ReadingTips/downloaded_media_files/play_some_videos.sh
@@ -3,9 +3,11 @@
 if [[ "$OS" == "Windows_NT" ]]; then
     # Windows 10 with Msys2
     soita="/mingw64/bin/mpv"    # Msys2: pacman -S mingw64/mingw-w64-x86_64-mpv
-else
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Ubuntu 20.04 and such
     soita='/usr/bin/mplayer'     # Ubuntu 20.04: apt install mplayer
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "jyy"
 fi
 
 echo 'video files: videos/'
