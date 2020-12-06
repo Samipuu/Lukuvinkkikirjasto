867c5fe66b808ade3e0c4b307dd5d5b5b54bb24c
Jussi Asp <jussi.asp@helsinki.fi>
2020-12-06 15:23:36 +0200

    database name set in properties

--- ReadingTips/src/test/java/readingtips/database/DaoTest.java
+++ ReadingTips/src/test/java/readingtips/database/DaoTest.java
@@ -4,6 +4,8 @@ import java.io.File;
 import org.junit.Before;
 import org.junit.Test;
 
+import readingtips.Main;
+
 public class DaoTest {
 
     // test x
@@ -11,14 +13,17 @@ public class DaoTest {
 
     }
 
+    private String myTestDatabase = "readingtips_DaoTest";
+
     @Before
     public void setUp() {
+        System.setProperty(Main.environment_database_name, myTestDatabase);
     }
 
     @Test
     public void alustaTietokantaTest() {
         // delete database
-        File file = new File("readingtips_test.mv.db");
+        File file = new File(myTestDatabase + ".mv.db");
         file.delete();
         // no database exists
         BookDao dao = new BookDao();
